fc8435a9e293f779b2abc579543a2097e90ba5d6
Karolina Charciunaite <karolina@Karolinas-MBP.lan>
2023-03-14 18:03:30 +0200

    latest updates

--- JavaScript/project1.js
+++ JavaScript/project1.js
@@ -11,8 +11,84 @@ $(document).ready(function() {
         itemsDesktop : false,
         itemsDesktopSmall : false,
         itemsTablet: false,
-        itemsMobile : false
+        itemsMobile : false,
+        loop: true,
+        autoplay: true,
    
     });
    
   });
+
+  //accordion
+
+  const accordion = document.getElementsByClassName('accordion');
+
+  for (i = 0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+  }
+
+  //button
+
+  let openBtn = document.getElementById('open-btn');
+  let modalContainer = document.getElementById('modal-container');
+  let closeBtn = document.getElementById('close-btn');
+
+  openBtn.addEventListener('click', function() {
+
+    modalContainer.style.display = 'block';
+  })
+
+  closeBtn.addEventListener('click', function() {
+
+    modalContainer.style.display = 'none';
+  })
+
+  window.addEventListener('click', function(e) {
+    
+    if (e.target === modalContainer) {
+      modalContainer.style.display = 'none';
+    }
+  })
+
+//second button
+  let openBtnOne = document.getElementById('open-btn-one');
+
+  openBtnOne.addEventListener('click', function() {
+
+    modalContainer.style.display = 'block';
+  })
+
+  closeBtn.addEventListener('click', function() {
+
+    modalContainer.style.display = 'none';
+  })
+
+  window.addEventListener('click', function(e) {
+    
+    if (e.target === modalContainer) {
+      modalContainer.style.display = 'none';
+    }
+  })
+
+  //third button
+
+  let openBtnTwo = document.getElementById('open-btn-two');
+
+  openBtnTwo.addEventListener('click', function() {
+
+    modalContainer.style.display = 'block';
+  })
+
+  closeBtn.addEventListener('click', function() {
+
+    modalContainer.style.display = 'none';
+  })
+
+  window.addEventListener('click', function(e) {
+    
+    if (e.target === modalContainer) {
+      modalContainer.style.display = 'none';
+    }
+  })
